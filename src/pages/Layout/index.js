0036bba0ff94903dbe989c83f1616dbd8a3b1db8
hai_an <1207783846@qq.com>
2023-06-11 16:03:03 +0800

    首页布局-使用antd的布局组件

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,13 +1,88 @@
 import React, { Component } from 'react'
-import { Pagination } from 'antd'
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from '@ant-design/icons'
+import { Breadcrumb, Layout, Menu } from 'antd'
 import styles from './index.module.less'
-// console.log(styles)
-// import './index.less'
-export default class Layout extends Component {
+const { Header, Content, Sider } = Layout
+const { SubMenu } = Menu
+const items1 = ['1', '2', '3'].map(key => ({
+  key,
+  label: `nav ${key}`,
+}))
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1)
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        }
+      }),
+    }
+  }
+)
+export default class LayoutComponent extends Component {
   render() {
     return (
-      <div className={styles.aa}>
-        <Pagination defaultCurrent={1} total={50} />
+      <div className={styles.layout}>
+        <Layout>
+          <Header className="header">
+            <div className="logo" />
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items1}
+            />
+          </Header>
+          <Layout>
+            <Sider width={200} className="site-layout-background">
+              <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{
+                  height: '100%',
+                  borderRight: 0,
+                }}
+                items={items2}
+              />
+            </Sider>
+            <Layout
+              style={{
+                padding: '0 24px 24px',
+              }}
+            >
+              <Breadcrumb
+                style={{
+                  margin: '16px 0',
+                }}
+              >
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb>
+              <Content
+                className="site-layout-background"
+                style={{
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 280,
+                }}
+              >
+                Content
+              </Content>
+            </Layout>
+          </Layout>
+        </Layout>
       </div>
     )
   }
