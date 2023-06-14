49ccb42245753b81433095d3136b191fccc23469
hai_an <1207783846@qq.com>
2023-06-14 17:27:17 +0800

    首页-侧边栏结构与样式的处理

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,86 +1,82 @@
+/*
+ * @Author: hai_an 1207783846@qq.com
+ * @Date: 2023-06-09 17:09:37
+ * @LastEditors: hai_an 1207783846@qq.com
+ * @LastEditTime: 2023-06-14 17:27:15
+ * @FilePath: \jikeyuan-pc\src\pages\Layout\index.js
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import React, { Component } from 'react'
-import {
-  LaptopOutlined,
-  NotificationOutlined,
-  UserOutlined,
-  LogoutOutlined,
-} from '@ant-design/icons'
+import { DiffOutlined, EditOutlined, LogoutOutlined, HomeOutlined } from '@ant-design/icons'
 import { Breadcrumb, Layout, Menu } from 'antd'
 import styles from './index.module.less'
 const { Header, Content, Sider } = Layout
-const { SubMenu } = Menu
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
+// 分离layout的侧边栏
+const tag = ['数据概览', '内容管理', '发布文章']
+const siderList = [HomeOutlined, DiffOutlined, EditOutlined].map((icon, index) => {
     const key = String(index + 1)
     return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        }
-      }),
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: tag[index],
     }
-  }
-)
+})
 export default class LayoutComponent extends Component {
-  render() {
-    return (
-      <div className={styles.layout}>
-        <Layout>
-          <Header className="header">
-            <div className="logo" />
-            <div className="profile">
-              <span>用户名</span>
-              <span>
-                <LogoutOutlined /> 退出
-              </span>
+    render() {
+        return (
+            <div className={styles.layout}>
+                <Layout>
+                    <Header className="header">
+                        <div className="logo pro" />
+
+                        <div className="profile">
+                            <span>用户名</span>
+                            <span>
+                                <LogoutOutlined />
+                                退出
+                            </span>
+                        </div>
+                    </Header>
+                    <Layout>
+                        <Sider width={200} className="site-layout-background">
+                            <Menu
+                                mode="inline"
+                                defaultSelectedKeys={['1']}
+                                defaultOpenKeys={['sub1']}
+                                style={{
+                                    height: '100%',
+                                    borderRight: 0,
+                                }}
+                                items={siderList}
+                            />
+                        </Sider>
+                        <Layout
+                            style={{
+                                padding: '0 24px 24px',
+                            }}>
+                            <Breadcrumb
+                                style={{
+                                    margin: '16px 0',
+                                }}>
+                                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                <Breadcrumb.Item>List</Breadcrumb.Item>
+                                <Breadcrumb.Item>App</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <Content
+                                className="site-layout-background"
+                                style={{
+                                    padding: 24,
+                                    margin: 0,
+                                    minHeight: 280,
+                                }}>
+                                Content{' '}
+                            </Content>
+                        </Layout>
+                    </Layout>
+                </Layout>
             </div>
-          </Header>
-          <Layout>
-            <Sider width={200} className="site-layout-background">
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{
-                  height: '100%',
-                  borderRight: 0,
-                }}
-                items={items2}
-              />
-            </Sider>
-            <Layout
-              style={{
-                padding: '0 24px 24px',
-              }}
-            >
-              <Breadcrumb
-                style={{
-                  margin: '16px 0',
-                }}
-              >
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
-              <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280,
-                }}
-              >
-                Content
-              </Content>
-            </Layout>
-          </Layout>
-        </Layout>
-      </div>
-    )
-  }
+        )
+    }
 }
